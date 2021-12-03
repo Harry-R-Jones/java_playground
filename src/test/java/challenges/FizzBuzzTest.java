2a80230c3224bf8bd49dcac594fed4ef1a6d64df
Harry Jones <harryrjones@yahoo.com>
2021-12-03 10:19:27 +0000

    Adjusted the test method to test any individual element rather than the entire Array (more suitable when testing larger numbers). Left original test as a comment on the previous line.

--- src/test/java/challenges/FizzBuzzTest.java
+++ src/test/java/challenges/FizzBuzzTest.java
@@ -1,17 +1,19 @@
 package challenges;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import challenges.Week1.FizzBuzz.FizzBuzz;
 import org.junit.jupiter.api.Test;
 
 public class FizzBuzzTest {
-    FizzBuzz fizzBuzz = new FizzBuzz(16);
+    //Create a FizzBuzz up to a limit
+    FizzBuzz fizzBuzz = new FizzBuzz(500);
 
     @Test
     public void fizzTest(){
-        String[] testArr = {"FizzBuzz","1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
-        assertArrayEquals(fizzBuzz.fizzBuzz(), testArr);
+        //String[] testArr = {"FizzBuzz","1","2","Fizz","4","Buzz","Fizz","7","8","Fizz","Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
+        assertEquals("Buzz", fizzBuzz.fizzBuzz()[400]);
 
     }
 }
