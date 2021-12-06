d6990248a48e98df256f198ff6ceac71ee33e336
Harry Jones <harryrjones@yahoo.com>
2021-12-06 12:35:16 +0000

    Refactored to allow for easier testing

--- src/main/java/challenges/Week2/palindrome_word_checker/PalindromeChecker.java
+++ src/main/java/challenges/Week2/palindrome_word_checker/PalindromeChecker.java
@@ -1,31 +1,31 @@
 package challenges.Week2.palindrome_word_checker;
 
 public class PalindromeChecker {
-    private String palindromeCandidate;
 
-    public PalindromeChecker(String palindromeCandidate) {
-        this.palindromeCandidate = palindromeCandidate;
+    public PalindromeChecker() {
+
     }
 
-    public boolean isPalindromic(){
+    public boolean isPalindromic(String palindromeCandidate){
         boolean isPalindrome = true;
         while(palindromeCandidate.length() > 1){
             if(palindromeCandidate.charAt(0) == palindromeCandidate.charAt(palindromeCandidate.length()-1)){
                 palindromeCandidate = palindromeCandidate.substring(1, palindromeCandidate.length()-1);
             } else {
                 isPalindrome = false;
+                palindromeCandidate = palindromeCandidate.substring(1, palindromeCandidate.length()-1);
             }
         }
 
         return isPalindrome;
     }
 
-    public static void main(String[] args) {
-        PalindromeChecker pC = new PalindromeChecker("makam");
-        if(pC.isPalindromic()){
-            System.out.println("True");
-        } else{
-            System.out.println("False");
-        }
-    }
+//    public static void main(String[] args) {
+//        PalindromeChecker pC = new PalindromeChecker("makam");
+//        if(pC.isPalindromic()){
+//            System.out.println("True");
+//        } else{
+//            System.out.println("False");
+//        }
+//    }
 }
