319bb6eb631ad3e4dfa1b70be450480bef3fba2b
Harry Jones <harryrjones@yahoo.com>
2021-12-06 15:10:17 +0000

    Swwapped the outer loop to while to increase efficiency

--- src/main/java/challenges/Week2/BubbleSort/BubbleSort.java
+++ src/main/java/challenges/Week2/BubbleSort/BubbleSort.java
@@ -2,20 +2,26 @@ package challenges.Week2.BubbleSort;
 
 public class BubbleSort {
 
-    public static int[] bubbleSort(int[] unsortedInts){
+    public static int[] bubbleSort(int[] unsortedInts) {
+        boolean sorted = false;
 
-        for(int i = 0; i < unsortedInts.length - 1; i++){
-            for(int j = 0; j < unsortedInts.length - 1; j++){
-                if(unsortedInts[j] > unsortedInts[j+1]){
+        while (!sorted) {
+            boolean changes = false;
+            for (int j = 0; j < unsortedInts.length - 1; j++) {
+                if (unsortedInts[j] > unsortedInts[j + 1]) {
                     int holder = unsortedInts[j];
                     unsortedInts[j] = unsortedInts[j + 1];
                     unsortedInts[j + 1] = holder;
+                    changes = true;
                 }
             }
+            if (!changes) {
+                sorted = true;
+            }
         }
-
         return unsortedInts;
-
     }
+
+
 
 }
